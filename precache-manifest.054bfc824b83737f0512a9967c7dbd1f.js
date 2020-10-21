c065cf0a19109b2163e5bf696423e3af15ec3ef1
amartya <amartya.sk@gmail.com>
2020-10-21 16:35:38 +0700

    Updates

---
+++ precache-manifest.054bfc824b83737f0512a9967c7dbd1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ca15fe03bb204889857ddcc436e149",
+    "url": "/ninetyseven-client/index.html"
+  },
+  {
+    "revision": "bcae92005f7f0cc0e5cc",
+    "url": "/ninetyseven-client/static/css/main.3f2ea0ed.chunk.css"
+  },
+  {
+    "revision": "9622b79949393c950d0c",
+    "url": "/ninetyseven-client/static/js/2.7cbfbacb.chunk.js"
+  },
+  {
+    "revision": "b28efdd56deb90065a6ed9c886c45c8b",
+    "url": "/ninetyseven-client/static/js/2.7cbfbacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcae92005f7f0cc0e5cc",
+    "url": "/ninetyseven-client/static/js/main.2e5abd59.chunk.js"
+  },
+  {
+    "revision": "09131b12ce646ea324e3",
+    "url": "/ninetyseven-client/static/js/runtime-main.7ec578c6.js"
+  }
+]);
